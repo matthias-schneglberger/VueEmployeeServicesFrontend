b1a93e3f1f2824ef67b7f39fe370d4f53d404401
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2021-05-25 20:32:44 +0200

    added get all Employees

---
+++ src/components/Employee.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-list-item-content>
+<!--    <v-list-item-title v-text="sampleText"></v-list-item-title>-->
+    <v-list-item-content style="padding: 0">
+      <v-container style="padding: 0">
+        <v-row>
+          <v-col cols="8" align-self="center">
+            {{name}}
+          </v-col>
+          <v-col cols="2">
+            <v-btn
+              icon
+              @click="show = !show"
+              width="50%"
+            >
+              Details anzeigen
+            </v-btn>
+
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            asdfasdfasdf
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-list-item-content>
+
+  </v-list-item-content>
+</template>
+
+<script>
+export default {
+  name: 'Employee',
+  data () {
+    return {
+      sampleText: 'test',
+      show: false,
+      center: { lat: 40.689247, lng: -74.044502 }
+    }
+  },
+  mounted () {
+    console.log('Employee')
+  },
+  props: {
+    name: String,
+    empId: String,
+    longitude: String,
+    latitude: String,
+    address: String
+  },
+  methods: {
+    showDetails: function () {
+      this.show = !this.show
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ShowEmployeesWithServices.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-list v-for="(employee, index) in employees" v-bind:key="employee.id">
+        <Employee :name="employee.name" :emp-id="employee.id"/>
+        <v-divider
+          v-if="index < employees.length - 1"
+          :key="index"
+        ></v-divider>
+    </v-list>
+    <Employee/>
+  </v-container>
+</template>
+
+<script>
+import Employee from '@/components/Employee'
+import axios from 'axios'
+export default {
+  name: 'ShowEmployeesWithServices',
+  components: {
+    Employee
+  },
+  data () {
+    return {
+      employees: []
+    }
+  },
+  mounted () {
+    this.reloadEmployees()
+  },
+  methods: {
+    reloadEmployees: function () {
+      axios
+        .get('http://localhost:9000/employees')
+        .then(response => (this.employees = response.data))
+        .then(_ => console.log(this.employees))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
